87a5397b86b549c5fe1a75d50d2761cb730ce125
EllieWalsch <elliewalsch4@gmail.com>
2023-03-08 12:17:13 -0600

    Hardcode NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+export default function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">About Me</a>
+        </li>
+        <li>
+          <a href="/portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="/resume">Resume</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
