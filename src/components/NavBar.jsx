aaf3796695ed69bbe332a3e48e1eb7773cbb94fb
EllieWalsch <elliewalsch4@gmail.com>
2023-03-08 12:45:28 -0600

    Center NavBar
    idk where I want this yet

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 export default function NavBar() {
   return (
     <nav>
-      <ul className="flex flex-row gap-5">
+      <ul className="flex flex-row justify-center gap-5">
         <li>
           <a href="/">About Me</a>
         </li>
