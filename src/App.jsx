8906ee567ec1349a5fa3aa9d326746631900a592
EllieWalsch <elliewalsch4@gmail.com>
2023-03-08 12:29:09 -0600

    Hardcode Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Ellie Walsch</h1>
       <NavBar />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+export default function Footer() {
+  return (
+    <footer className="absolute inset-x-0 bottom-0 h-16">
+      <ul>
+        <li>
+          <a
+            href="https://github.com/EllieWalsch"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/ellie-walsch-691622251/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
