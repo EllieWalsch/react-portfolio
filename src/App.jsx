84494f4999620b5b78156bf971610d816bbb8994
EllieWalsch <elliewalsch4@gmail.com>
2023-03-08 13:44:19 -0600

    WIP make Title dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import NavBar from "./components/NavBar";
-import Title from "./components/Title";
 import AboutMe from "./components/AboutMe";
 import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import Title from "./components/Title";
 
 function App() {
   return (
     <>
       <h1 className="mt-5 ml-2 text-5xl font-bold">Ellie Walsch</h1>
       <NavBar />
-      <Title />
+      <Title text={"Test"} />
       <AboutMe />
       <Footer />
     </>
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,13 @@
-export default function Title() {
+import PropTypes from "prop-types";
+
+export default function Title({ text }) {
   return (
     <header className="flex justify-center">
-      <h2 className="mt-10 w-2/4 text-2xl">Title</h2>
+      <h2 className="mt-10 w-2/4 text-2xl">{text}</h2>
     </header>
   );
 }
+
+Title.propTypes = {
+  text: PropTypes.string.isRequired,
+};
