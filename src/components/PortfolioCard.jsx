93e9d303ef5753341bf1164dc5ebcf710db83c50
EllieWalsch <elliewalsch4@gmail.com>
2023-03-09 13:31:19 -0600

    Hardcode all cards

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,13 +1,33 @@
 export default function PortfolioCard() {
   return (
-    <main className="mt-10 flex justify-center">
+    <main className="mx-16 mb-16 mt-10 grid grid-cols-3 grid-rows-2 justify-items-center gap-2">
       <figure>
         <figcaption>FBI Profiler</figcaption>
+        <img src="src\images\fbi.png" alt="" width="400" height="186" />
+      </figure>
+      <figure>
+        <figcaption>JATE</figcaption>
+        <img src="src\images\JATE.png" alt="" width="400" height="186" />
+      </figure>
+      <figure>
+        <figcaption>Password Generator</figcaption>
+        <img src="src\images\password.png" alt="" width="400" height="186" />
+      </figure>
+      <figure>
+        <figcaption>Coding Quiz</figcaption>
+        <img src="src\images\quiz.png" alt="" width="400" height="186" />
+      </figure>
+      <figure>
+        <figcaption>Work Day Scheduler</figcaption>
+        <img src="src\images\schedule.png" alt="" width="400" height="186" />
+      </figure>
+      <figure>
+        <figcaption>Food Finder</figcaption>
         <img
-          src="src\images\FBI-profiler.png"
+          src="src\images\Food-finder3.png"
           alt=""
-          width="500"
-          height="363"
+          width="400"
+          height="186"
         />
       </figure>
     </main>
