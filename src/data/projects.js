81c0c149dea63768b66921ba0097cfa289880b3a
EllieWalsch <elliewalsch4@gmail.com>
2023-03-09 15:42:08 -0600

    Create data file for all projects

---
+++ src/data/projects.js
@@ -0,0 +1,45 @@
+export default [
+  {
+    name: "FBI Profiler",
+    github: "https://github.com/EllieWalsch/fbi-profiler",
+    deployed: "https://fbi-profiler.herokuapp.com/login",
+    id: 1,
+    src: "src/images/fbi.png",
+  },
+  {
+    name: "JATE",
+    github: "https://github.com/EllieWalsch/text-editor",
+    deployed: "https://calm-oasis-46520.herokuapp.com/",
+    id: 2,
+    src: "src/images/JATE.png",
+  },
+  {
+    name: "Password Generator",
+    github: "https://github.com/EllieWalsch/password-generator",
+    deployed: "https://elliewalsch.github.io/password-generator/",
+    id: 3,
+    src: "src/images/password.png",
+  },
+  {
+    name: "Coding Quiz",
+    github: "https://github.com/EllieWalsch/coding-quiz",
+    deployed: "https://elliewalsch.github.io/coding-quiz/",
+    id: 4,
+    src: "src/images/quiz.png",
+  },
+  {
+    name: "Work Day Scheduler",
+    github: "https://github.com/EllieWalsch/work-day-scheduler",
+    deployed: "https://elliewalsch.github.io/work-day-scheduler/",
+    id: 5,
+    src: "src/images/schedule.png",
+  },
+  {
+    name: "Food Finder",
+    github: "https://github.com/eabaynes/Food-Finder",
+    deployed:
+      "https://63857389528963204b4dc037--startling-biscuit-4e7633.netlify.app/",
+    id: 6,
+    src: "src/images/Food-finder3.png",
+  },
+];
