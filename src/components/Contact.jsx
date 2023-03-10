22b8a19efdd902bf8e9d272ef0f823caafc74ce2
EllieWalsch <elliewalsch4@gmail.com>
2023-03-10 14:20:39 -0600

    Validate email in form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,35 @@
+import checkEmail from "../utils/validateEmail";
+import React from "react";
+
 export default function Contact() {
+  const [email, setEmail] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState("");
+
+  const handleInputChange = (event) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = event;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "email") {
+      setEmail(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (event) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!checkEmail(email)) {
+      setErrorMessage("Email is invalid");
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+    }
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setEmail("");
+  };
+
   return (
     <main className="mt-10 flex justify-center">
       <form>
@@ -10,7 +41,12 @@ export default function Contact() {
         <div>
           <label htmlFor="Email">Email Address</label>
           <div>
-            <input type="email" />
+            <input
+              value={email}
+              name="email"
+              onChange={handleInputChange}
+              type="email"
+            />
           </div>
         </div>
 
@@ -21,10 +57,19 @@ export default function Contact() {
           </div>
         </div>
 
-        <button className="text-center" type="submit">
+        <button
+          className="rounded-lg bg-lightpink px-5 text-center"
+          type="submit"
+          onClick={handleFormSubmit}
+        >
           Send
         </button>
       </form>
+      {errorMessage && (
+        <div>
+          <p>{errorMessage}</p>
+        </div>
+      )}
     </main>
   );
 }
