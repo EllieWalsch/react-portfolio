7481d6625bc5598e93d814901deceac4f76dd9b3
EllieWalsch <elliewalsch4@gmail.com>
2023-03-10 15:27:57 -0600

    Check for name

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,6 +4,7 @@ import React from "react";
 export default function Contact() {
   const [email, setEmail] = React.useState("");
   const [message, setMessage] = React.useState("");
+  const [name, setName] = React.useState("");
 
   const handleInputChange = (event) => {
     // Getting the value and name of the input which triggered the change
@@ -14,20 +15,29 @@ export default function Contact() {
     if (inputType === "email") {
       setEmail(inputValue);
     }
+    if (inputType === "name") {
+      setName(inputValue);
+    }
   };
 
   const handleFormSubmit = (event) => {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     event.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    // First we check to see if the email is not valid or if the name is empty. If so we set an error message to be displayed on the page.
     if (!checkEmail(email)) {
       setMessage("Email is invalid");
       // We want to exit out of this code block if something is wrong so that the user can correct it
       return;
     }
+    if (!name) {
+      setMessage("Name is required");
+      return;
+    }
+
     // If everything goes according to plan, we want to clear out the input after a successful registration.
     setEmail("");
+    setName("");
     setMessage("Thank you for reaching out!");
   };
 
@@ -36,7 +46,12 @@ export default function Contact() {
       <form>
         <label htmlFor="Name">Name</label>
         <div>
-          <input type="text" />
+          <input
+            value={name}
+            name="name"
+            onChange={handleInputChange}
+            type="text"
+          />
         </div>
 
         <div>
