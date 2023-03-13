945d4d27c7f4c6b888234be0cb5e36dbcd2f2605
EllieWalsch <elliewalsch4@gmail.com>
2023-03-13 12:11:09 -0500

    Style portfolio for mobile

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,6 +1,6 @@
 export default function PortfolioCard({ projectData }) {
   return (
-    <main className="mx-16 mt-10 grid grid-cols-3 grid-rows-2 justify-items-center gap-2 pb-3">
+    <main className="mx-16 mt-10 grid grid-cols-1 justify-items-center gap-2 pb-3 sm:grid-cols-3 sm:grid-rows-2">
       {projectData.map((project) => {
         return (
           <figure className="hover:opacity-50">
