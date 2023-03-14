adfba8637639d4a4a2a671b56a5d20503ea8d769
EllieWalsch <elliewalsch4@gmail.com>
2023-03-14 09:44:42 -0500

    Add skills

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -8,14 +8,14 @@ export default function Resume() {
       >
         Download
       </a>
-      <p className="mt-11 w-2/4 text-center">
-        RESUME: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+      <p className="mt-11 w-2/4 text-xl font-semibold text-green sm:justify-self-start">
+        Skills:
+      </p>
+      <p className="w-2/4 sm:justify-self-start">
+        Committed customer service, strong written and verbal skills, fast
+        learner, proficient computer skills, well versed in Microsoft Office,
+        Google Workspace, and Vinsolutions. Experience with Javascript, MERN,
+        and React.
       </p>
     </main>
   );
